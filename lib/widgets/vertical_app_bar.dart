26b334837aca297e5cf0cbcce194c31a6405917b
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-05-08 10:13:25 +0600

    Changed vertical app bar style

--- lib/widgets/vertical_app_bar.dart
+++ lib/widgets/vertical_app_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class VerticalAppBar extends StatefulWidget {
@@ -12,41 +14,93 @@ class _VerticalAppBarState extends State<VerticalAppBar> {
 
   @override
   Widget build(BuildContext context) {
+    final colorTheme = Theme.of(context).colorScheme;
     return NavigationRail(
-      selectedIndex: _selectedIndex,
-      groupAlignment: -1,
-      backgroundColor: Theme.of(context).primaryColor,
-      onDestinationSelected: (int index) {
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-      labelType: NavigationRailLabelType.all,
-      destinations: const [
-        NavigationRailDestination(
-          icon: Icon(Icons.dashboard_outlined),
-          selectedIcon: Icon(Icons.dashboard),
-          label: Text('Dashboard'),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.computer_outlined),
-          selectedIcon: Icon(Icons.computer),
-          label: Text('Items'),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.account_circle_outlined),
-          selectedIcon: Icon(Icons.account_circle),
-          label: Text('Users'),
-        ),
-      ],
-      useIndicator: true,
-      indicatorShape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(5))),
-      selectedIconTheme: Theme.of(context).iconTheme.copyWith(size: 32),
-      unselectedIconTheme:
-          Theme.of(context).iconTheme.copyWith(color: Colors.white, size: 32),
-      unselectedLabelTextStyle: const TextStyle(color: Colors.white),
-      selectedLabelTextStyle: const TextStyle(color: Colors.white),
-    );
+        selectedIndex: _selectedIndex,
+        groupAlignment: -1,
+        backgroundColor: colorTheme.onSurface,
+        onDestinationSelected: (int index) {
+          setState(() => _selectedIndex = index);
+          log('index $index');
+        },
+        labelType: NavigationRailLabelType.none,
+        destinations: [
+          CustomNavigationRailDestination(
+            icon: Icons.dashboard_outlined,
+            selectedIcon: Icons.dashboard,
+            label: 'Dashboard',
+            isSelected: _selectedIndex == 0,
+            highlightColor: colorTheme.onPrimary,
+          ),
+          CustomNavigationRailDestination(
+            icon: Icons.computer_outlined,
+            selectedIcon: Icons.computer,
+            label: 'Items',
+            isSelected: _selectedIndex == 1,
+            highlightColor: colorTheme.onPrimary,
+          ),
+          CustomNavigationRailDestination(
+            icon: Icons.account_circle_outlined,
+            selectedIcon: Icons.account_circle,
+            label: 'Users',
+            isSelected: _selectedIndex == 2,
+            highlightColor: colorTheme.onPrimary,
+          ),
+          CustomNavigationRailDestination(
+            icon: Icons.account_circle_outlined,
+            selectedIcon: Icons.account_circle,
+            label: 'Report Statistics',
+            isSelected: _selectedIndex == 3,
+            highlightColor: colorTheme.onPrimary,
+          ),
+        ],
+        useIndicator: false,
+        selectedIconTheme: Theme.of(context)
+            .iconTheme
+            .copyWith(color: colorTheme.surface, size: 48),
+        unselectedIconTheme: Theme.of(context)
+            .iconTheme
+            .copyWith(color: colorTheme.surfaceVariant, size: 48));
   }
+}
+
+class CustomNavigationRailDestination extends NavigationRailDestination {
+  CustomNavigationRailDestination({
+    required IconData icon,
+    required IconData selectedIcon,
+    required String label,
+    required Color highlightColor,
+    required bool isSelected,
+  }) : super(
+          icon: Container(
+            decoration: BoxDecoration(
+              color: isSelected
+                  ? Colors.white.withOpacity(0.5)
+                  : Colors.transparent,
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+            ),
+            child: Column(
+              children: [
+                Icon(selectedIcon, color: Colors.white),
+                Text(label, style: const TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
+          selectedIcon: Container(
+            width: 200,
+            decoration: BoxDecoration(
+              color: isSelected
+                  ? highlightColor.withOpacity(0.3)
+                  : Colors.transparent,
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+            ),
+            child: Column(
+              children: [
+                Icon(selectedIcon),
+                Text(label, style: const TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
+          label: Text(label),
+        );
 }
